8a6e239a99c5f945a1cf19483313ba21e3c101fc
Fareed Sodha <muhammadfareed5045@gmail.com>
2024-11-25 15:13:09 +0500

    Login API Created

--- inspira/src/app/api/user/login/route.js
+++ inspira/src/app/api/user/login/route.js
@@ -8,8 +8,31 @@ export async function POST(request) {
     const obj = await request.json();
     console.log("login obj=>", obj);
 
-    
+
     const user = await UserModal.findOne({ email: obj.email });
-    console.log(user)
-    return Response.json('Working On Login')
+
+    if (!user) return Response.json(
+        { error: true, msg: "User Not Find" },
+        { status: 404 }
+    )
+
+    const isPasswordMatch = await bcrypt.compare(obj.password, user.password);
+
+    if (!isPasswordMatch) return Response.json(
+        { error: true, msg: "Password Is Not Valid" },
+        { status: 400 }
+
+    )
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_KEY);
+
+
+    console.log("isPasswordMatch=>", isPasswordMatch);
+
+    return Response.json({
+        error: false,
+        msg: "User Login Successfully",
+        user: newUser,
+        token,
+    });
+
 }
